ff40125735cb40335cff3fb4ccc1397e52427802
Deepak Rawat <119871101+deepakrawat0007@users.noreply.github.com>
2022-12-23 15:58:30 +0530

    Update SuccessOrder.jsx

--- src/componants/order/CreateOrder/SuccessOrder/SuccessOrder.jsx
+++ src/componants/order/CreateOrder/SuccessOrder/SuccessOrder.jsx
@@ -7,7 +7,7 @@ export default function SuccessOrder(props){
     return<div className="success_holder">
         <div className="success_component">
             <img className="success_img" src={`./images/icon-for-createorder/tick.png`} alt="tick"/>
-            <h4>Your order is <br/> successfully</h4>
+            <h4>Your order is <br/> successfully placed</h4>
             <p>You can track the delivery in the <br/> "Orders" section</p>
             <button onClick={()=>navigate("/order")} >Go to Orders</button>
         </div>
